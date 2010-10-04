f368e3b6dc74224c8fe70176799f857b142ab030
Ken Faulkner <ken.faulkner@gmail.com>
2010-10-04 18:31:34 +1100

    init

---
+++ README.txt
@@ -0,0 +1,1 @@
+AKKA test ground
---
+++ src/neo/server.scala
@@ -0,0 +1,27 @@
+package server
+
+import net.lag.configgy.Configgy
+import net.lag.logging.Logger
+
+object server
+{
+  
+  def main(args: Array[String]) =
+  {
+  	var port = 9999
+	  if ( args.length >=1 )
+		{
+		  port = args(0).toInt
+		}
+		
+		if ( args.length >= 2)
+		{
+		  var configFile = args(1)
+		  Configgy.configure( configFile )
+    
+		}
+  
+    println( "port is " + port.toString() )
+
+  }
+}
